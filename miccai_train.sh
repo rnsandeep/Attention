dce9f66eb3565b6327765455e80d09d38e6950f7
Naga Sandeep Ramachandruni <sandeep@heallo.ai>
2019-06-26 13:24:38 +0530

    added training code

---
+++ miccai_train.sh
@@ -0,0 +1,25 @@
+
+max_epoch=60
+classes=8
+dataset='/home/cureskin/HealloAI/miccai/trainval_split'
+
+exp='miccai_attention_focal'
+meanfile='mean_std_darkcircle.npy'
+
+####### Training Network #########################
+
+#python train_attention.py $dataset $exp $meanfile 299 $classes $max_epoch
+
+########## TEsting NEtwork #######################
+max_size=224
+min_size=224
+for epoch in `seq 0 $max_epoch`
+do
+   for size in `seq $min_size 20 $max_size`	
+   do	   
+       f="_checkpoint.pth.tar"
+       echo "epoch: ", $epoch, "resize_size: ", $size
+       python test_attention.py $dataset $exp/$epoch$f $meanfile 224 $size $classes out_$exp
+   done    
+done
+
